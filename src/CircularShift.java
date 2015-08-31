4f95134bc7e29f25d892d7bcdc5fcbed6c7f78c0
BenedictSu <Benedict_S_S@u.nus.edu>
2015-08-31 22:15:13 +0800

    Add CircularShifter
    Add Class CircularShifter which compute the unsorted required list

---
+++ src/CircularShift.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+* CircularShift
+* 
+* It computes the list of Key words where the circular titles do not start
+* with an ignored word
+*/
+public class CircularShift {
+    
+    private ArrayList input;
+    private HashSet ignoredWords;
+    
+    public CircularShift (ArrayList userInput, HashSet userIgnoredWords) {
+        input = userInput;
+        ignoredWords = userIgnoredWords;      
+        
+    }
+    
+    
+    
+}
