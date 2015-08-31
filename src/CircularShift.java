6972f4362254aca79ae1ab22162b9976f7b5c84a
BenedictSu <Benedict_S_S@u.nus.edu>
2015-08-31 22:45:02 +0800

    complete checkIgnore
    - complete the checking of key words
    - refactor

--- src/CircularShift.java
+++ src/CircularShift.java
@@ -2,40 +2,49 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
-* CircularShift
-* 
-* It computes the list of Key words where the circular titles do not start
-* with an ignored word
-*/
+ * CircularShift
+ * 
+ * It computes the list of Key words where the circular titles do not start
+ * with an ignored word
+ */
 public class CircularShift {
-    
+
     private ArrayList<String> input;
     private ArrayList<String> unsortedList;
     private HashSet<String> ignoredWords;
     String[] userInputArray;
-    
-    public CircularShift (ArrayList<String> userInput, HashSet<String> userIgnoredWords) {
+
+    public CircularShift(ArrayList<String> userInput,
+            HashSet<String> userIgnoredWords) {
         input = userInput;
-        ignoredWords = userIgnoredWords;      
+        ignoredWords = userIgnoredWords;
         computeList();
     }
-    
-    private void computeList () {
+
+    private void computeList() {
         unsortedList = new ArrayList<String>();
         for (int i = 0; i < input.size(); i++) {
             userInputArray = splitUserInput(input.get(i));
-            checkIgnore(0);
-            
+            checkIgnore(userInputArray.length, 0, "");
+
         }
     }
-    
+
     private String[] splitUserInput(String sentence) {
         return sentence.trim().split("\\s+");
     }
-    
-    private void checkIgnore (int position) {
-        
-        
+
+    private String checkIgnore(int lastPosition, int position, String frontWords) {
+        String secondPart = "";
+        if (position != lastPosition - 1) {
+            secondPart =
+                    checkIgnore(lastPosition, position + 1, frontWords + " "
+                            + userInputArray[position]);
+        }
+        if (ignoredWords.contains(userInputArray[position])) {
+            unsortedList.add(userInputArray[position] + " " + secondPart + " "
+                    + frontWords);
+        }
+        return userInputArray[position] + " " + secondPart;
     }
-    
 }
